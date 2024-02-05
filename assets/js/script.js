2b1e516599e991d76c3f926b9ec9c8059980c2da
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-02-06 00:45:57 +0330

    create calcPace method

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,12 @@ class Running extends Workout {
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.place = this.duration / this.distance;
+    return this.place;
   }
 }
 
